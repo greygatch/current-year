df9c68fbdaee8c1fbec5a8b4c03431f26a9229a4
Evan Gatchell <egatchell@changehealthcare.com>
2017-03-08 14:38:45 -0600

    fin

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('testing')
+const rootDiv = document.getElementById('root');
+
+setInterval(function(){
+  var timeNow = new Date();
+  var currentYear = timeNow.getFullYear();
+  var time = timeNow.toLocaleTimeString();
+  var display = 'Seriously? Get with the program, it\'s ' + time + ' in ' + currentYear + '.';
+  rootDiv.innerHTML = display;
+}, 1000)
